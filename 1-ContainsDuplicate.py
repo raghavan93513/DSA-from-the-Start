16d336fb910c18bfd4ce597852b1e33d899c2673
raghavan93513 <raghavanv2002@gmail.com>
2026-04-02 15:50:42 +0530

    02/04/2016

---
+++ 1-ContainsDuplicate.py
@@ -0,0 +1,21 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_set = set()
+        for i in range(len(nums)):
+            if nums[i] in my_set:
+                return True
+            else:
+                my_set.add(nums[i])
+        return False
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) < len(nums):
+            return True
+        return False
